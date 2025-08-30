0998ba604773b62780f0ccf997c88002c30bbffd
Roshan Dalvi <roshandalvi16@gmail.com>
2025-08-30 22:02:35 +0530

    CRUD operations

--- 28-08-25 CRUD/basic_Revies.sql
+++ 28-08-25 CRUD/basic_Revies.sql
@@ -3,11 +3,11 @@ CREATE DATABASE employee;
 CREATE TABLE employee_details(eid int PRIMARY KEY, ename varchar(50), eemail varchar(50), eage int, egender varchar(50), esalary int, eaddress varchar(50), ecity varchar(50));
 
 INSERT INTO employee_details (eid, ename, eemail, eage, esalary, eaddress, ecity) VALUES  
-(1, 'raju', 'raju@gmail.com', 20,'Male', 50000, 'sadashiv peth', 'pune');  
+(1, 'raju', 'raju@gmail.com', 20,'Male', 50000, 'sadashiv peth', 'pune');  /*inserting*/
 
-SELECT * FROM employee_details;
+SELECT * FROM employee_details;  /*read*/
 
-UPDATE employee_details set esalary=60000 WHERE eid=1;
+UPDATE employee_details set esalary=60000 WHERE eid=1;  /*update alter*/
 
-DELETE  
+DELETE employee_details; /*delete*/
 
