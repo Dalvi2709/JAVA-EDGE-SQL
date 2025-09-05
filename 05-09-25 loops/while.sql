cd9a76ae815548124656e643109463e6e305871b
Roshan Dalvi <roshandalvi16@gmail.com>
2025-09-05 10:58:41 +0530

    checkaddition function

--- 05-09-25 loops/while.sql
+++ 05-09-25 loops/while.sql
@@ -14,3 +14,22 @@ RETURN result;
 END
 
 
+
+CREATE FUNCTION `checkaddition` (num int)
+RETURNS INTEGER
+BEGIN
+declare result int;
+declare i int;
+
+set result = 1;
+set i = num;
+
+lable1 : while i >= 1 do
+set resulet = result + i;
+set i = i - 1;
+END while lable1;
+
+RETURN result;
+END
+
+
