fde58d5dbd7499327cbe9bc4e41dce8f23c115b4
Roshan Dalvi <roshandalvi16@gmail.com>
2025-09-05 21:19:33 +0530

    PROCEDURE

---
+++ 05-09-25 loops/procedure.sql
@@ -0,0 +1,17 @@
+CREATE PROCEDURE `insert_user`(in p_id int(10), in p_name varchar(45))
+    DETERMINISTIC
+BEGIN
+insert into user_register(id,name) values (p_id,p_name);
+END
+
+
+call insert_user(101,"raju");
+
+
+CREATE PROCEDURE `remove_user`(in p_id int(10))
+    DETERMINISTIC
+BEGIN
+delete from user_register where p_id = id;
+END
+
+call remove_user(101);
