6f5928001b5bd2b2c94e228ef37134b376f0a0cf
Roshan Dalvi <roshandalvi16@gmail.com>
2025-09-05 10:33:33 +0530

    while loop syntax

---
+++ 05-09-25 loops/while.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION `checkfactorial` (num int)
+RETURNS INTEGER
+BEGIN
+declare result int;
+declare i int;
+set result = 1;
+set i = num;
+
+lable1 : while i >= 1 do
+set resulet = result * i;
+set i = i - 1;
+END while lable1;
+RETURN result;
+END
+
+
